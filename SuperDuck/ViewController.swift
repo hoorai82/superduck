9ed47a666a074a33bbeb774856bb6e166b42dbfc
박준성 <blue-byuli@daum.net>
2016-02-23 22:03:18 +0900

    Initial Commit

---
+++ SuperDuck/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperDuck
+//
+//  Created by byuli on 2016. 2. 23..
+//  Copyright © 2016년 byuli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
